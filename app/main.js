77ec792103a2eb503089c8f2ecef93f0e4631238
easierbycode <daniel@easierbycode.com>
2017-07-18 13:06:08 -0700

    add mobile / desktop device ready listeners

--- app/main.js
+++ app/main.js
@@ -4,4 +4,13 @@ import GAME from './constants/game';
 
 let game = new Game(GAME.WIDTH, GAME.HEIGHT, 'app', Phaser.CANVAS);
 let manager = new StateManager(game);
-manager.start();
+
+function start() {
+  manager.start();
+}
+
+if (window.cordova) {
+  document.addEventListener('deviceready', start, false);
+} else {
+  document.addEventListener('DOMContentLoaded', start, false);
+}
